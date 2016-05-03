b7883aacfa5732714792d9c99af2db4e3c934ae0
Sergiu <sbodiu@pivotal.io>
2016-05-03 23:46:09 +0800

    add spring cloud services

--- src/main/java/io/pivotal/apac/TraderServerApplication.java
+++ src/main/java/io/pivotal/apac/TraderServerApplication.java
@@ -4,7 +4,18 @@ import lombok.Data;
 import org.joda.money.Money;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
+import org.springframework.stereotype.Component;
 
+@EnableJpaAuditing
+@EnableJpaRepositories
+@EnableDiscoEurekaClient
+@EnableHystrix
 @SpringBootApplication
 public class TraderServerApplication {
 
